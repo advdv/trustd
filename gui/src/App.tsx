9bf15c564116d291ff752a301da601573b5c77b7
avdv <advanderveer@gmail.com>
2025-03-30 16:39:51 +0200

    flow with random nodes

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -1,34 +1,65 @@
-import { useState } from "react";
-import trustdLogo from "/trustd.svg";
-import "./App.css";
+import {
+  ReactFlow,
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+} from "@xyflow/react";
 
-function App() {
-  const [count, setCount] = useState(0);
+import "@xyflow/react/dist/style.css";
 
-  return (
-    <>
-      <div>
-        <img src={trustdLogo} className="logo" alt="Vite logo" />
-      </div>
-      <h1 className="text-3xl font-bold underline">Trustd</h1>
-      <div className="card">
-        <button
-          type="button"
-          onClick={() => {
-            setCount((count) => count + 1);
-          }}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+// generateRandomGraph will generate a random graph.
+function generateRandomGraph(nodeCount: number, edgeCount: number) {
+  const nodes = Array.from({ length: nodeCount }, (_, i) => ({
+    id: (i + 1).toString(),
+    position: { x: Math.random() * 1000, y: Math.random() * 1000 },
+    data: { label: `Node ${(i + 1).toString()}` },
+  }));
+
+  const edges = Array.from({ length: edgeCount }, (_, i) => {
+    let source, target;
+    do {
+      source = Math.ceil(Math.random() * nodeCount).toString();
+      target = Math.ceil(Math.random() * nodeCount).toString();
+    } while (source === target); // ensure source != target
+
+    return {
+      id: `e${i.toString()}-${source}-${target}`,
+      source,
+      target,
+      label: Math.random().toFixed(4), // random decimal
+    };
+  });
+
+  return { nodes, edges };
 }
 
-export default App;
+// declare initial nodes and edges.
+const { nodes: initialNodes, edges: initialEdges } = generateRandomGraph(
+  100,
+  50,
+);
+
+// render the diagram.
+export default function App() {
+  const [nodes, , onNodesChange] = useNodesState(initialNodes);
+  const [edges, , onEdgesChange] = useEdgesState(initialEdges);
+
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        fitView
+        defaultEdgeOptions={{ type: "step" }}
+      >
+        <Controls />
+        <MiniMap />
+        <Background gap={12} size={1} />
+      </ReactFlow>
+    </div>
+  );
+}
